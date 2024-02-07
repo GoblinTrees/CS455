2078c348419a23b308f3d717378fb1b4babdd053
connorparrott <connorparrott123@gmail.com>
2024-02-07 13:37:04 -0700

    revert turn changes

--- 2_battlebots/keyboard controls.py
+++ 2_battlebots/keyboard controls.py
@@ -1,8 +1,10 @@
 import tkinter as tk
 from maestro import Controller                                                     
 
-L_MOTORS = 0
-R_MOTORS = 1
+MOTORS = 0
+TURN = 1
+# L_MOTORS = 0
+# R_MOTORS = 1
 BODY = 2
 HEADTURN = 3
 HEADTILT = 4
@@ -70,38 +72,49 @@ class KeyControl():
 
     def arrow(self, key):
         print(key.keycode)
+
+        # backwards
         if key.keycode == 116:
             self.motors += 200
             if(self.motors > 7900):
                 self.motors = 7900
             print(self.motors)
-            self.tango.setTarget(L_MOTORS, self.motors)
-            self.tango.setTarget(R_MOTORS, self.motors)
+            self.tango.setTarget(MOTORS, self.motors)
+            # self.tango.setTarget(R_MOTORS, self.motors)
+
+        # forwards
         elif key.keycode == 111:
             self.motors -= 200
             if(self.motors < 1510):
                 self.motors = 1510
             print(self.motors)
-            self.tango.setTarget(L_MOTORS, self.motors)
-            self.tango.setTarget(R_MOTORS, self.motors)
+            self.tango.setTarget(MOTORS, self.motors)
+            # self.tango.setTarget(R_MOTORS, self.motors)
+        
+        # right
         elif key.keycode == 114:
             self.turn += 200
             if(self.turn > 7400):
                 self.turn = 7400
             print(self.turn)
-            self.tango.setTarget(R_MOTORS, self.turn)
+            self.tango.setTarget(TURN, self.turn)
+        
+        # left
         elif key.keycode == 113:
             self.turn -= 200
             if(self.turn <2110):
                 self.turn = 2110
             print(self.turn)
-            self.tango.setTarget(L_MOTORS, self.turn)
+            self.tango.setTarget(TURN, self.turn)
         
+        # escape (estop)
         elif key.keycode == 9:
             self.motors = 6000
             self.turn = 6000
-            self.tango.setTarget(R_MOTORS, self.motors)
-            self.tango.setTarget(L_MOTORS, self.turn)
+            self.tango.setTarget(MOTORS, self.motors)
+            self.tango.setTarget(TURN, self.turn)
+            # self.tango.setTarget(R_MOTORS, self.motors)
+            # self.tango.setTarget(L_MOTORS, self.turn)
 
     def l_arm(self, key):
         print(key.keycode)
