83e69c8feb22bf49b2e4115b44212a70848d0b52
connorparrott <connorparrott123@gmail.com>
2024-02-09 12:43:29 -0700

    forward

--- 2_battlebots/keyboard controls.py
+++ 2_battlebots/keyboard controls.py
@@ -113,8 +113,8 @@ class KeyControl():
                 if (self.r_motors > 7900):
                     self.r_motors = 7900
                 # since wheels are controlled separately they are reversed (one is positive forward while the other is negative forward)
-                temp = abs(self.r_motors - 6000)
-                self.l_motors = 6000 - temp
+                temp = abs(abs(self.r_motors) - 6000)
+                self.l_motors = 6000 + temp
 
                 print(self.r_motors)
                 print(self.l_motors)
@@ -128,7 +128,8 @@ class KeyControl():
                 if (self.r_motors < 1510):
                     self.r_motors = 1510
                 temp = abs(abs(self.r_motors) - 6000)
-                self.l_motors = 6000 + temp
+                self.l_motors = 6000 - temp
+
                 print(self.r_motors)
                 print(self.l_motors)
                 self.tango.setTarget(L_MOTORS, self.l_motors)
