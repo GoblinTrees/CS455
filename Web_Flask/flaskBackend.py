bcc3f0a2f610d4c1e65d3666c64a9931ce25e7b5
Forrest G <forrestcgile@gmail.com>
2024-02-09 10:59:25 -0700

    forrest's updates 2/9

---
+++ Web_Flask/flaskBackend.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/check")
+def checkWorking():
+    return "Hello from Flask"
+
+@app.route("/joyData")
+def checkWorking():
+    return "Hello from Flask"
+
+@app.route("/wheels/<int:value>")
+def getWheelsValue(value):
+    print("v", str(value))
+    #if value == 9:
+        #value = "worked"
+    return str(value)
+
+#The user facing page for controlling the tango
+@app.route('/')
+def home():
+   return render_template('index.html')
+
+#the backend for updated values to be sent to
+@app.route('/recieving', methods=['GET', 'POST'])
+def parse_request():
+    data = request.values  # data is empty
+
+
+app.run(host="0.0.0.0", port=3245, debug=True)
+
+
+
