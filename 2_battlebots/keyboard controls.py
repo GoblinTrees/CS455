d2ce31c262c39f9100cfa069203373ff20f4a557
Forrest G <forrestcgile@gmail.com>
2024-02-09 11:10:29 -0700

    Forrest made a mistake, didnt apply changes. fixing now

--- 2_battlebots/keyboard controls.py
+++ 2_battlebots/keyboard controls.py
@@ -1,5 +1,15 @@
 import tkinter as tk
-from maestro import Controller                                                     
+from maestro import Controller
+from Xlib import XK
+
+keycode_to_keysym = {}
+for name in dir(XK):
+    if name.startswith("XK_"):
+        keycode = getattr(XK, name)
+        keysym = XK.keysym_to_string(keycode)
+        if keysym:
+            keycode_to_keysym[keycode] = keysym
+
 
 MOTORS = 0
 TURN = 1
@@ -45,87 +55,86 @@ class KeyControl():
         self.r_wrist = 6000
         self.r_fingers = 6000
 
-
-        
-        
-    def head(self,key):
-        print(key.keycode)
-        if key.keycode == 38:
+    def head(self, key):
+        print(key.keysym)
+        if key.keysym == "Up":
             self.headTurn += 200
-            if(self.headTurn > 7900):
+            if self.headTurn > 7900:
                 self.headTurn = 7900
             self.tango.setTarget(HEADTURN, self.headTurn)
-        elif key.keycode ==40:
+        elif key.keysym == "Down":
             self.headTurn -= 200
-            if(self.headTurn < 1510):
+            if self.headTurn < 1510:
                 self.headTurn = 1510
             self.tango.setTarget(HEADTURN, self.headTurn)
-        elif key.keycode == 25:
+        elif key.keysym == "Right":
             self.headTilt += 200
-            if(self.headTilt > 7900):
+            if self.headTilt > 7900:
                 self.headTilt = 7900
             self.tango.setTarget(HEADTILT, self.headTilt)
-        elif key.keycode == 39:
+        elif key.keysym == "Left":
             self.headTilt -= 200
-            if(self.headTilt < 1510):
+            if self.headTilt < 1510:
                 self.headTilt = 1510
             self.tango.setTarget(HEADTILT, self.headTilt)
 
     def waist(self, key):
-        print(key.keycode)
-        
-        if key.keycode == 54:
+        keysym = keycode_to_keysym.get(key.keycode)
+        print(keysym)
+
+        if keysym == "Right":
             self.body += 200
-            if(self.body > 7900):
+            if (self.body > 7900):
                 self.body = 7900
             self.tango.setTarget(BODY, self.body)
             print("waist right")
-        elif key.keycode == 52:
+        elif keysym == "Left":
             self.body -= 200
-            if(self.body < 1510):
+            if (self.body < 1510):
                 self.body = 1510
             self.tango.setTarget(BODY, self.body)
-            print ('waist left')
+            print('waist left')
 
     def arrow(self, key):
-        print(key.keycode)
+        keysym = keycode_to_keysym.get(key.keycode)
+        print(keysym)
 
         # backwards
-        if key.keycode == 114:
+        if keysym == "Down":
             self.motors += 200
-            if(self.motors > 7900):
+            if (self.motors > 7900):
                 self.motors = 7900
             print(self.motors)
             self.tango.setTarget(MOTORS, self.motors)
             # self.tango.setTarget(R_MOTORS, self.motors)
 
         # forwards
-        elif key.keycode == 113:
+        elif keysym == "Up":
             self.motors -= 200
-            if(self.motors < 1510):
+            if (self.motors < 1510):
                 self.motors = 1510
             print(self.motors)
             self.tango.setTarget(MOTORS, self.motors)
             # self.tango.setTarget(R_MOTORS, self.motors)
-        
+
         # right
-        elif key.keycode == 111:
+        elif keysym == "Right":
             self.turn += 200
-            if(self.turn > 7400):
+            if (self.turn > 7400):
                 self.turn = 7400
             print(self.turn)
             self.tango.setTarget(TURN, self.turn)
-        
+
         # left
-        elif key.keycode == 116:
+        elif keysym == "Left":
             self.turn -= 200
-            if(self.turn <2110):
+            if (self.turn < 2110):
                 self.turn = 2110
             print(self.turn)
             self.tango.setTarget(TURN, self.turn)
-        
+
         # escape (estop)
-        elif key.keycode == 9:
+        elif keysym == "Escape":
             self.default = 6000
             self.tango.setTarget(MOTORS, self.default)
             self.tango.setTarget(TURN, self.default)
@@ -145,111 +154,33 @@ class KeyControl():
             self.tango.setTarget(R_WRIST, self.default)
             self.tango.setTarget(R_FINGERS, self.default)
 
-
-            # self.tango.setTarget(R_MOTORS, self.motors)
-            # self.tango.setTarget(L_MOTORS, self.turn)
-
     def l_arm(self, key):
-        print(key.keycode)
-        if key.keycode == 29:
+        keysym = keycode_to_keysym.get(key.keycode)
+        print(keysym)
+        if keysym == "s":
             print("Shoulder")
             self.l_shoulder += 200
-            if(self.l_shoulder <2110):
+            if (self.l_shoulder < 2110):
                 self.l_shoulder = 2110
             print(self.l_shoulder)
             self.tango.setTarget(L_SHOULDER, self.l_shoulder)
-        elif key.keycode == 30:
-            print("Bicep")
-            self.l_bicep += 200
-            if(self.l_bicep <2110):
-                self.l_bicep = 2110
-            print(self.l_bicep)
-            self.tango.setTarget(L_BICEP, self.l_bicep)
-        elif key.keycode == 31:
-            print("Elbow")
-            self.l_elbow += 200
-            if(self.l_elbow <2110):
-                self.l_elbow = 2110
-            print(self.l_elbow)
-            self.tango.setTarget(L_ELBOW, self.l_elbow)
-        elif key.keycode == 32:
-            print("Forearm")
-            self.l_forarm += 200
-            if(self.l_forarm <2110):
-                self.l_forarm = 2110
-            print(self.l_forarm)
-            self.tango.setTarget(L_FORARM, self.l_forarm)
-        elif key.keycode == 33:
-            print("Wrist")
-            self.l_wrist += 200
-            if(self.l_wrist <2110):
-                self.l_wrist = 2110
-            print(self.l_wrist)
-            self.tango.setTarget(L_WRIST, self.l_wrist)
-        elif key.keycode == 34:
-            print("claw")
-            self.l_fingers += 200
-            if(self.l_fingers <2110):
-                self.l_fingers = 2110
-            print(self.l_fingers)
-            self.tango.setTarget(L_FINGERS, self.l_fingers)
-        elif key.keycode == 42:
-            print("Shoulder")
-            self.l_shoulder -= 200
-            if(self.l_shoulder <2110):
-                self.l_shoulder = 2110
-            print(self.l_shoulder)
-            self.tango.setTarget(L_SHOULDER, self.l_shoulder)
-        elif key.keycode == 43:
-            print("Bicep")
-            self.l_bicep -= 200
-            if(self.l_bicep <2110):
-                self.l_bicep = 2110
-            print(self.l_bicep)
-            self.tango.setTarget(L_BICEP, self.l_bicep)
-        elif key.keycode == 44:
-            print("Elbow")
-            self.l_elbow -= 200
-            if(self.l_elbow <2110):
-                self.l_elbow = 2110
-            print(self.l_elbow)
-            self.tango.setTarget(L_ELBOW, self.l_elbow)
-        elif key.keycode == 45:
-            print("Forearm")
-            self.l_forarm -= 200
-            if(self.l_forarm <2110):
-                self.l_forarm = 2110
-            print(self.l_forarm)
-            self.tango.setTarget(L_FORARM, self.l_forarm)
-        elif key.keycode == 46:
-            print("Wrist")
-            self.l_wrist -= 200
-            if(self.l_wrist <2110):
-                self.l_wrist = 2110
-            print(self.l_wrist)
-            self.tango.setTarget(L_WRIST, self.l_wrist)
-        elif key.keycode == 47:
-            print("claw")
-            self.l_fingers -= 200
-            if(self.l_fingers <2110):
-                self.l_fingers = 2110
-            print(self.l_fingers)
-            self.tango.setTarget(L_FINGERS, self.l_fingers)
     
     def r_arm(self, key):
         print(key.keycode)
-        if key.keycode == 42:
-            print("Shoulder")
-        elif key.keycode == 43:
-            print("Bicep")
-        elif key.keycode == 44:
-            print("Elbow")
-        elif key.keycode == 45:
-            print("Forearm")
-        elif key.keycode == 46:
-            print("Wrist")
-        elif key.keycode == 47:
-            print("claw")
+        print(key.keysym)
+        match key:
+            case 42:
+                print("Shoulder")
+            case 43:
+                print("Bicep")
+            case 44:
+                print("Elbow")
+            case 45:
+                print("Forearm")
+            case 46:
+                print("Wrist")
+            case 47:
+                print("claw")
 
 win = tk.Tk()
 keys = KeyControl(win)
