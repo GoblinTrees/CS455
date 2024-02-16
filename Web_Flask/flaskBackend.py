02c3cefbab490b675fbd2bfa5ef0957768374008
Forrest G <forrestcgile@gmail.com>
2024-02-15 23:27:21 -0700

    dynamic ip addressing, human readable dict values. good to integrate with maestro file

--- Web_Flask/flaskBackend.py
+++ Web_Flask/flaskBackend.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,27 +6,54 @@ app = Flask(__name__)
 def checkWorking():
     return "Hello from Flask"
 
-
-
-@app.route("/wheels/<int:value>")
-def getWheelsValue(value):
-    print("v", str(value))
-    #if value == 9:
-        #value = "worked"
-    return str(value)
-
-#The user facing page for controlling the tango
 @app.route('/')
 def home():
-   return render_template('index.html')
+    print(request.host)
+    ip_add = request.host
+    return render_template('index.html', ip_address=ip_add)
 
-#the backend for updated values to be sent to
-@app.route('/recieving', methods=['GET', 'POST'])
-def parse_request():
-    data = request.values  # data is empty
+@app.route('/control', methods=['POST'])
+def control_robot():
+    print("control input recieved...")
+    # Retrieve data from the form
+    HeadTilt = int(request.form['slider1'])
+    HeadTurn = int(request.form['slider2'])
+    LShoulder = int(request.form['slider3'])
+    LBicep= int(request.form['slider4'])
+    Lelbow = int(request.form['slider5'])
+    Lwrist = int(request.form['slider6'])
+    Lclaw = int(request.form['slider7'])
+    RShoulder = int(request.form['slider8'])
+    RBicep = int(request.form['slider9'])
+    Relbow = int(request.form['slider10'])
+    Rwrist = int(request.form['slider11'])
+    Rclaw = int(request.form['slider12'])
+    Waist = int(request.form['slider13'])
+
+    body_dict = {
+        "Headtilt": HeadTilt,
+        "Headturn": HeadTurn,
+        "Lshoulder": LShoulder,
+        "Lbicep": LBicep,
+        "Lelbow": Lelbow,
+        "Lwrist": Lwrist,
+        "Lclaw": Lclaw,
+        "RShoulder": RShoulder,
+        "Rbicep": RBicep,
+        "Relbow": Relbow,
+        "Rwrist": Rwrist,
+        "Rclaw": Rclaw,
+        "Waist": Waist,
+    }
+    print(body_dict)
+
+    # Process the data (you can add your robot control logic here)
 
 
-app.run(host="0.0.0.0", port=3245, debug=True)
+
+    # You can send a response if needed
+    return "Received the control data successfully!"
 
 
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5245, debug=True)
