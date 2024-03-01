8b1a91905bfe8e8e46402d8da86acac78ef55133
Rordog <70729720+Rordog@users.noreply.github.com>
2024-03-01 12:20:14 -0700

    Finish Project4Client.py

--- Project4Client.py
+++ Project4Client.py
@@ -5,6 +5,9 @@ import time
 host = 'localhost'
 port = 5000
 
+global token
+token = 110
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
 
@@ -19,41 +22,42 @@ script = {0: [("rec", "Hi", 1), ("rec", "I was just going to say the same thing 
               ("send", "What are the odds. Two robots run into to each other from the same state,and the same town, and the same room, with the same name?", 0)]}
 
 def receive_token():
-    return int.from_bytes(s.recv(3), 'big')
-    
+    global token
+    token = int.from_bytes(s.recv(3), 'big')    
 
 def send_token(token):
     s.send(token.to_bytes(3, 'big'))
 
-def execute_script(scriptid, token, script):
+def execute_script(scriptid, script):
+    global token
     if scriptid in script:
         actions = script[scriptid]
         if actions:
             action, data, dest = actions[0]
             if token == 111:
+                #never receiving token 111
                 token = 110
                 print("Client: ", data)
-                send_token(token)
-                token = 0
+                send_token(110)
                 actions.pop(0)
             elif token == 110:
                 print("Server: ", data)
                 token = 111
-                send_token(token)
-                token = 0
+                send_token(111)
                 actions.pop(0)
-
-    return token
-
-# Define the initial token value
-token = 0
 
 # Define a loop to run the program
 while True:
-    token = receive_token()
-    token = execute_script(1, token, script)
-    token = receive_token()
-    token = execute_script(0, token, script)
+    #disconnect on start of second loop
+    execute_script(0, script)
+    if len(script[1]) == 0:
+        time.sleep(5)
+        break
+    time.sleep(1)
+    receive_token()
+    execute_script(1, script)
+    time.sleep(1)
+    receive_token()
 # Close the connection with the client
 s.close()
 
