292bde665ad76246f7f9e72972fbca79de4b4d4d
Forrest G <forrestcgile@gmail.com>
2024-03-08 10:55:47 -0700

    Attempt to run personality with moved Kore attempt 2.25

--- Kore.py
+++ Kore.py
@@ -398,7 +398,8 @@ animation_refresh_seconds = 0.01
 # main executable funtion
 if __name__ == "__main__":
     kore = Kore()
-    kore.boot()  # Submit Flask app to ThreadPoolExecutor
+    #The Flask start
+    kore.exec.submit(app.run(host="0.0.0.0", port=5245, debug=True))
     animation_window = create_animation_window()
     animation_canvas = create_animation_canvas(animation_window)
 
