4dc39ee219d8a4c0f047756c0ce15a6cab4879db
connorparrott <connorparrott123@gmail.com>
2024-03-15 16:01:27 -0600

    temp

--- 3_personality_eyes_talk/temp.py
+++ 3_personality_eyes_talk/temp.py
@@ -55,9 +55,6 @@ class Robot:
         elif event.keysym == 'v':
             self.is_idle = True
             self.isTalking = True
-            self.canvas.update()
-            self.talk("I am a robot")
-            self.talk_animation()
             # self.isTalking = False
             
 
@@ -133,21 +130,36 @@ class Robot:
             time.sleep(0.1)
 
     def talk_animation(self):
+        # Draw head
+        head = self.canvas.create_oval(70, 250, 230, 350, fill="lightgray")
 
-        # Draw smile
+        # Draw eyes
+        eye_left = self.canvas.create_oval(90, 280, 110, 320, fill="white")
+        eye_right = self.canvas.create_oval(190, 280, 210, 320, fill="white")
+
+        # Draw pupils
+        pupil_left = self.canvas.create_oval(95, 290, 105, 310, fill="black", tags="pupil_left")
+        pupil_right = self.canvas.create_oval(195, 290, 205, 310, fill="black", tags="pupil_right")
+
+        # Draw mouth
+        mouth = self.canvas.create_oval(120, 320, 180, 340, fill="black")
+
         for _ in range(3):  # Adjust the number of iterations to control the movement of the mouth
-            # Open mouth
-            self.canvas.create_arc(120, 320, 180, 340, start=0, extent=180, style=tk.ARC)
-            self.root.update()
-            time.sleep(0.2)  # Adjust the duration to control the speed of the animation
+            # Increase size of mouth
+            for i in range(5):
+                self.canvas.scale(mouth, 1.05, 1.05)  # Scale mouth by 5% in both x and y directions
+                self.root.update()
+                time.sleep(0.05)  # Adjust the duration to control the speed of the animation
 
-            # Close mouth
-            self.canvas.create_arc(120, 320, 180, 335, start=0, extent=180, style=tk.ARC)
-            self.root.update()
-            time.sleep(0.2)  # Adjust the duration to control the speed of the animation
+            # Decrease size of mouth
+            for i in range(5):
+                self.canvas.scale(mouth, 0.95, 0.95)  # Scale mouth by 5% in both x and y directions
+                self.root.update()
+                time.sleep(0.05)  # Adjust the duration to control the speed of the animation
 
         # Delete objects
-        self.canvas.delete(head, eye_left, eye_right, pupil_left, pupil_right)
+        self.canvas.delete(head, eye_left, eye_right, pupil_left, pupil_right, mouth)
+
 
 
     def move_stick_figure(self, x=100, y=200):
@@ -208,7 +220,12 @@ class Robot:
             elif self.isTalking:
                 self.canvas.delete("all")
                 self.talk_animation()
+                self.talk("I am a robot")
                 self.root.update()
+
+    
+            
+            
 
     def talk(self, words):
         # Animation for talking (you can customize this)
