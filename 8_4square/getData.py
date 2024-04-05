72243ab563211f5e7cbfbbb6d1e8d36dcfdc7731
Connor Parrott <connorparrott123@gmail.com>
2024-04-05 12:39:24 -0600

    Update getData.py

--- 8_4square/getData.py
+++ 8_4square/getData.py
@@ -23,7 +23,9 @@ while searching:
                 # mc 0f 00000663 000005a3 00000512 000004cb  ffffffff ffffffff ffffffff 095f c1 00146fb7 a0:0 22be
                 # 0  1  2        3        4        5        6        7        8        9        10   11 12       13   14
                 data=ser.readline()
-                print(data[0])
+                print(data)
+                for i in range data.length()
+                        print(data[i]
                 
         finally:
                 print("error")
