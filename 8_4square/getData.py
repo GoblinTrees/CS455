e34cfc3d4e395a0b583898e77b4a638046924ad0
Connor Parrott <connorparrott123@gmail.com>
2024-04-05 12:56:33 -0600

    Update getData.py

--- 8_4square/getData.py
+++ 8_4square/getData.py
@@ -24,8 +24,6 @@ while searching:
                 # 0  1  2        3        4        5        6        7        8        9        10   11 12       13   14
                 data=ser.readline()
                 print(data)
-                for i in range(9):
-                        print(data[i])
                 
         finally:
                 print("error")
