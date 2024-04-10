c7e15c61a37f6275203183d6a1c613347bab7c2c
Connor Parrott <connorparrott123@gmail.com>
2024-04-10 13:21:34 -0600

    Update getData.py

--- 8_4square/getData.py
+++ 8_4square/getData.py
@@ -40,10 +40,10 @@ try:
         robot.tango.setTarget(robot.R_MOTORS, r_motors)
         try:
             temp = ser.readline()
-            print(temp)
+            print("line1: ", temp)
             data = str(ser.readline()).split(",")
-            print(data)
-            print(data[2])
+            print("line2: ", data)
+            #print(data[2])
         except Exception as e:
             print("Error processing data:", e)
             break  # Exit the loop if an error occurs
