61673e544a4ee317f40149f34031652541b229b4
Connor Parrott <connorparrott123@gmail.com>
2024-04-10 13:07:37 -0600

    Update getData.py

--- 8_4square/getData.py
+++ 8_4square/getData.py
@@ -13,12 +13,6 @@ class Robot:
         
 robot = Robot()
 searching = True
-foundPylon = False
-pylon = None
-numData = 0
-target = None
-previousHighest = 0
-missionSuccess = False
 
 try:
     ser = serial.Serial()
@@ -31,50 +25,24 @@ try:
     ser.open()
     time.sleep(1)
     while searching:
-        if foundPylon:
-            # rotate for .5 seconds
-            r_motors = 7000
-            tango.setTarget(robot.R_MOTORS, robot.r_motors)
-            time.sleep(.5)
-            r_motors = 6000
-            tango.setTarget(robot.R_MOTORS, robot.r_motors)
-
-            # Drive straight based on distance calculated in pylon[target]
-            r_motors = 6600
-            l_motors = 5800
-            tango.setTarget(robot.L_MOTORS, robot.l_motors)
-            tango.setTarget(robot.R_MOTORS, robot.r_motors)
-            time.sleep(3)  # Adjust the time to drive as needed
-            r_motors = 6000
-            l_motors = 6000
-            tango.setTarget(robot.L_MOTORS, robot.l_motors)
-            tango.setTarget(robot.R_MOTORS, robot.r_motors)
-
-            searching = False
-
-        else:
-            try:
-                data = ser.readline().decode().strip()
-                data_parts = data.split()
-                if len(data_parts) == 15:  # Check if the data has all parts
-                    # Extract relevant data
-                    pylon_data = [int(data_parts[2]), int(data_parts[3]), int(data_parts[4]), int(data_parts[5])]
-                    
-                    if numData == 9:
-                        print("Found 10 data points")
-                        pylon = np.mean(dataPoints, axis=0)
-                        target = np.argmax(pylon)
-                        foundPylon = True
-                        previousHighest = pylon[target]
-                    else:
-                        # Accumulate data points
-                        numData += 1
-                        dataPoints[numData] = pylon_data
-            except Exception as e:
-                print("Error processing data:", e)
-                break  # Exit the loop if an error occurs
-            finally:
-                print("Mission Successful")
+        l_motors = 7000
+        r_motors = 5000
+        tango.setTarget(L_MOTORS, l_motors)
+        tango.setTarget(R_MOTORS, r_motors)
+        time.sleep(1)
+        l_motors = 6000
+        r_motors = 6000
+        tango.setTarget(L_MOTORS, l_motors)
+        tango.setTarget(R_MOTORS, r_motors)
+        try:
+            data = ser.readline().decode().strip()
+            data_parts = data.split()
+            print(data_parts)
+        except Exception as e:
+            print("Error processing data:", e)
+            break  # Exit the loop if an error occurs
+        finally:
+            print("Mission Successful")
 except Exception as e:
     print("ERROR IN FIRST TRY OF GET DISTANCES:", e)
     pass
