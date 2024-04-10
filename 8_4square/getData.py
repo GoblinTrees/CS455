de43b43e23ef2fb0550f56f7f854939e88fbc2c7
Connor Parrott <connorparrott123@gmail.com>
2024-04-10 12:55:06 -0600

    Update getData.py

--- 8_4square/getData.py
+++ 8_4square/getData.py
@@ -22,13 +22,13 @@ missionSuccess = False
 
 try:
     ser = serial.Serial()
-    ser.open()
     ser.port = '/dev/ttyUSB0'
     ser.baudrate = 115200
     ser.bytesize = serial.EIGHTBITS
     ser.parity = serial.PARITY_NONE
     ser.stopbits = serial.STOPBITS_ONE
     ser.timeout = 1
+    ser.open()
     time.sleep(1)
     ser.close()
     while searching:
