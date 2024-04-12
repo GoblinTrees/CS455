7ed82e9d0338d7707a66892729e6e068f84f4191
Connor Parrott <connorparrott123@gmail.com>
2024-04-12 12:21:37 -0600

    Update getData.py

--- 8_4square/getData.py
+++ 8_4square/getData.py
@@ -15,6 +15,7 @@ robot = Robot()
 searching = True
 count = 0
 quadrant = False
+quadNum = 5
 arr = np.zeros((10, 4))
 
 try:
@@ -45,7 +46,10 @@ try:
             data = str(ser.readline()).split(",")
             #print("line2: ", data) # decimal values
             if quadrant:
-                
+                arr[0, 0] = str(data[1])
+                arr[0, 1] = str(data[2])
+                arr[0, 2] = str(data[3])
+                arr[0, 3] = str(data[4])
                 l_motors = 5000
                 robot.tango.setTarget(robot.L_MOTORS, l_motors)
                 time.sleep(.25)
@@ -60,6 +64,13 @@ try:
                 r_motors = 6000
                 robot.tango.setTarget(robot.L_MOTORS, l_motors)
                 robot.tango.setTarget(robot.R_MOTORS, r_motors)
+                temp = ser.readline()
+                data = str(ser.readline()).split(",")
+                arr[1, 0] = str(data[1])
+                arr[1, 1] = str(data[2])
+                arr[1, 2] = str(data[3])
+                arr[1, 3] = str(data[4])
+                if arr[0, quadNum] <
             if str(data[1]) == 'null' or str(data[2]) == 'null' or str(data[3]) == 'null' or str(data[4]) =='null':
                 print("bad data, trying again")
             else:
@@ -73,12 +84,16 @@ try:
             min = np.argmin(arr[0])
             #print(min)
             if min % 4 == 0:
+                quadNum = 0
                 print("quadrant 0")
             if min % 4 == 1:
+                quadNum = 1
                 print("quadrant 1")
             if min % 4 == 2:
+                quadNum = 2
                 print("quadrant 2")
             if min % 4 == 3:
+                quadNum = 3
                 print("quadrant 3")
             quadrant = True
         except Exception as e:
