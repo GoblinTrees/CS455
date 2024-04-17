e03d09e9856811f72928da06c324270cb073163a
Forrest G <forrestcgile@gmail.com>
2024-04-17 13:28:57 -0600

    alt 4

--- 8_4square/speed.py
+++ 8_4square/speed.py
@@ -1,9 +1,11 @@
 from maestro import Controller
 import time
 
-tango = Controller()
-tango.setTarget(1, 5800)
-tango.setTarget(0, 6600)
-time.sleep(1)
-tango.setTarget(1, 6000)
-tango.setTarget(0, 6000)
+if "__name__" == "__main__":
+    tango = Controller()
+    tango.setTarget(1, 5800)
+    tango.setTarget(0, 6600)
+    time.sleep(1)
+    tango.setTarget(1, 6000)
+    tango.setTarget(0, 6000)
+
