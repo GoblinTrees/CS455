cf26c8417193dfe0ec1f5f2a9ce2df59894834e4
Connor Parrott <connorparrott123@gmail.com>
2024-04-17 13:16:46 -0600

    Update speed.py

---
+++ 8_4square/speed.py
@@ -0,0 +1,9 @@
+import Controller from Maestro
+import time
+
+tango = Controller()
+tango.setTarget(1, 5800)
+tango.setTarget(0, 6600)
+time.sleep(1)
+tango.setTarget(1, 6000)
+tango.setTarget(0, 6000)
