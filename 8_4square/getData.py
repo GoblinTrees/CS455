f7d40134e8110baaf89bcf20a1581f3ce2b343b5
Forrest G <forrestcgile@gmail.com>
2024-04-18 13:56:35 -0600

    alt1061

--- 8_4square/getData.py
+++ 8_4square/getData.py
@@ -107,7 +107,7 @@ def findQuadrant():
 def turn90():
     l_motors = 5000
     robot.tango.setTarget(robot.L_MOTORS, l_motors)
-    t.sleep(.5)
+    t.sleep(.65)
     l_motors = 6000
     robot.tango.setTarget(robot.L_MOTORS, l_motors)
 
