6746748db390bca5b558bd6a598c4c3c9bd0965c
Forrest G <forrestcgile@gmail.com>
2024-04-18 12:44:19 -0600

    alt 98

--- 8_4square/getData.py
+++ 8_4square/getData.py
@@ -45,7 +45,7 @@ def findDistances():
             print("ConInt: ", confidenceInt)
             temp = ser.readline()
             data = str(ser.readline()).split(",")
-            print(data)
+            print(data[1])
             if str(data[1]) == 'null' or str(data[2]) == 'null' or str(data[3]) == 'null' or str(data[4]) == 'null':
                 print("bad data1, trying again")
             elif str(data[1]) == 'nan' or str(data[2]) == 'nan' or str(data[3]) == 'nan' or str(data[4]) == 'nan':
