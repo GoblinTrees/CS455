5efbc0974e5186947d6668d836edd8f23c042959
Connor Parrott <connorparrott123@gmail.com>
2024-04-22 11:45:31 -0600

    Update 9_Ultrasound.py

--- 8_4square/9_Ultrasound.py
+++ 8_4square/9_Ultrasound.py
@@ -5,8 +5,8 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 # set GPIO Pins
-TRIG_PIN = 14
-ECHO_PIN = 17
+TRIG_PIN = 24
+ECHO_PIN = 23
 
 # set trig as output anc ECHO as input
 GPIO.setup(TRIG_PIN, GPIO.OUT)
