d2548aea7913d24cdb712ebb6100c4b781293dcd
Connor Parrott <connorparrott123@gmail.com>
2024-04-22 11:47:52 -0600

    Update 9_Ultrasound.py

--- 8_4square/9_Ultrasound.py
+++ 8_4square/9_Ultrasound.py
@@ -14,33 +14,31 @@ GPIO.setup(ECHO_PIN, GPIO.IN)
 
 def getDistance():
     # set TRIG to LOW for a short time to ensure a clean signal
-    GPIO.output(TRIG_PIN, False)
-    time.sleep(0.1)
-
-    # send a 10us pulse to trigger
-    GPIO.output(TRIG_PIN, True)
-    time.sleep(0.00001)
-    GPIO.output(TRIG_PIN, False)
-
-    # Measure the time it takes for the echo to return
-    while GPIO.input(ECHO_PIN) == 0:
-        if (time.time() - timeout) > 3:  # timeout after 1 sec
-            print("Timeout occured while recieving ...")
+    GPIO.setmode(GPIO.BCM)
+    trigPin = 14
+    echoPin = 17
+    GPIO.setup(trigPin, GPIO.OUT)
+    GPIO.setup(echoPin, GPIO.IN)
+    GPIO.output(trigPin, False)
+    t.sleep(.1)
+    GPIO.output(trigPin, True)
+    t.sleep(.00001)
+    GPIO.output(trigPin, False)
+    timeout = t.time()
+    while GPIO.input(echoPin) == 0:
+        if (t.time() - timeout) > 3:
+            print("timeout during echo")
             return None
-    pulse_start = time.time()
-    timeout = time.time()
-
-    while GPIO.input(ECHO_PIN) == 1:
-        if (time.time() - timeout) > 3:  # timeout after 1 sec
-            print("Timeout occured while recieving ...")
+    pulseStart = t.time()
+    timeout = t.time()
+    while(GPIO.input(echoPin) == 1:
+        if(time.time() - timeout) > 3:
+            print("timeout while receiving echo")
             return None
-    pulse_end = time.time()
-
-    pulse_duration = pulse_end - pulse_start
-
-    #speed of sound is 343 m/s
-    distance = pulse_duration * 17150
-    distance = round(distance,2)
+    pulseEnd = t.time()
+    pulseDuration = pulseEnd - pulseStart
+    distance = pulseDuration * 17150
+    distance = round(distance, 2)
     return distance
     
 print("Diatance: ", getDistance())
