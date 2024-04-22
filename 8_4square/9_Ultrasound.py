3e9d77cc1b5e58b8d81d455bee844ef9723e2b2b
Connor Parrott <connorparrott123@gmail.com>
2024-04-22 11:29:57 -0600

    Update 9_Ultrasound.py

--- 8_4square/9_Ultrasound.py
+++ 8_4square/9_Ultrasound.py
@@ -13,25 +13,23 @@ GPIO.setup(TRIG_PIN, GPIO.OUT)
 GPIO.setup(ECHO_PIN, GPIO.IN)
 
 
-def getDistance(){
+def getDistance():
     # set TRIG to LOW for a short time to ensure a clean signal
     GPIO.output(TRIG_PIN, False)
+    time.sleep(0.1)
 
+    # send a 10us pulse to trigger
+    GPIO.output(TRIG_PIN, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIG_PIN, False)
 
-time.sleep(0.1)
-
-# send a 10us pulse to trigger
-GPIO.output(TRIG_PIN, True)
-time.sleep(0.00001)
-GPIO.output(TRIG_PIN, False)
-
-# Measure the time it takes for the echo to return
-while GPIO.input(ECHO_PIN) == 0:
-    if (time.time() - timeout) > 3:  # timeout after 1 sec
-        print("Timeout occured while recieving ...")
-        return None
-    pulse_start = time.time()
-    timeout = time.time()
+    # Measure the time it takes for the echo to return
+    while GPIO.input(ECHO_PIN) == 0:
+        if (time.time() - timeout) > 3:  # timeout after 1 sec
+            print("Timeout occured while recieving ...")
+            return None
+        pulse_start = time.time()
+        timeout = time.time()
 
     while GPIO.input(ECHO_PIN) == 1:
         if (time.time() - timeout) > 3:  # timeout after 1 sec
