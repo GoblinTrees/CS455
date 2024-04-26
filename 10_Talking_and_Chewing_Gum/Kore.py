2c6467fccdb70d17a2e9cc8c0afd61deece587ee
Forrest G <forrestcgile@gmail.com>
2024-04-26 10:37:40 -0600

    8 - updated

--- 10_Talking_and_Chewing_Gum/Kore.py
+++ 10_Talking_and_Chewing_Gum/Kore.py
@@ -295,34 +295,38 @@ def pose():
     kore.update(startpose)
 
     while not stop_flag:
-        random_number = random.randint(1, 3)
-        print("RandNum: "+str(random_number)+"\n")
-        time.sleep(random_number)
-        random_pose_key2 = Pl.get_random_pose_key(Pl.all_poses)
-        print("Random starter pose from 'all_poses': ", random_pose_key2)
-        endpose = Pl.all_poses.get(random_pose_key2)
+        try:
+            random_number = random.randint(1, 3)
+            print("RandNum: "+str(random_number)+"\n")
+            time.sleep(random_number)
+            random_pose_key2 = Pl.get_random_pose_key(Pl.all_poses)
+            print("Random starter pose from 'all_poses': ", random_pose_key2)
+            endpose = Pl.all_poses.get(random_pose_key2)
 
-        # random choice of transition
-        random_number = random.randint(1, 3)
-        if random_number == 1:  # Go direct
-            kore.update(endpose)
-            time.sleep(random.randint(10, 30))
-            startpose = kore.send_values()
-            continue
-        elif random_number == 2:  # Go fivesteps
-            steps: list = Pl.fiveStep(startpose, endpose)
-            for s in steps:
-                kore.update(s)
-                time.sleep(50)
+            # random choice of transition
+            random_number = random.randint(1, 3)
+            if random_number == 1:  # Go direct
+                kore.update(endpose)
+                time.sleep(random.randint(10, 30))
                 startpose = kore.send_values()
+                continue
+            elif random_number == 2:  # Go fivesteps
+                steps: list = Pl.fiveStep(startpose, endpose)
+                for s in steps:
+                    kore.update(s)
+                    time.sleep(50)
+                    startpose = kore.send_values()
+                continue
+            elif random_number == 3:  # Go back after a random amount of time
+                kore.update(endpose)
+                time.sleep(random.randint(10, 30))
+                kore.update(startpose)
+                continue
+            else:
+                continue
+        except:
             continue
-        elif random_number == 3:  # Go back after a random amount of time
-            kore.update(endpose)
-            time.sleep(random.randint(10, 30))
-            kore.update(startpose)
-            continue
-        else:
-            continue
+
 
 # End of Bootup function
 
