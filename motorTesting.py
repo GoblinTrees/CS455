1a0ff740a04c597b3e5cbbb5cb1ecb28b448695b
Forrest G <forrestcgile@gmail.com>
2024-05-03 11:43:18 -0600

    motorTesting.py (19)

--- motorTesting.py
+++ motorTesting.py
@@ -190,7 +190,7 @@ class Robot:
                     solutions.remove(sol)
 
         if len(solutions) == 1:                 #only one solution should remain, set the XY coordinates to it
-            xy = [solutions[0].get(X), solutions[0].get(Y)]
+            xy = [round([0].get(X),2), round(solutions[0].get(Y)),2]
             print(f"XY: "+str(xy[0]) + "-"+str(xy[1]))
 
             return
