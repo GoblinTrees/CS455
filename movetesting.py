767648f34cbe9fa295e3d906ff3a8db9bad327b6
Connor Parrott <connorparrott123@gmail.com>
2024-05-04 10:30:21 -0600

    Update movetesting.py

--- movetesting.py
+++ movetesting.py
@@ -367,7 +367,7 @@ def rot_angle(self, angle):
         
 if __name__ == "__main__":
     robot = Robot()
-    for i in range(2):
+    for i in range(3):
         rot_angle(robot, 34)
     
     # mapThread = threading.Thread(target=robot.startmapping())
