d27a943a281f92f7a7316fc83ed888d0a24ea6f2
Forrest G <forrestcgile@gmail.com>
2024-05-06 10:12:20 -0600

    main fixes

--- Project12Final.py
+++ Project12Final.py
@@ -160,7 +160,9 @@ class Robot:
 
         for sol in solutions:  # check every key-value for X,Y, and if they're less than 0 or greater than the side length of the large square then toss the data
             for key in sol.keys():
-                sol[key] = float(sol[key].real)
+                sol[key] = sol[key].real
+                sol[key] = float(sol[key])
+
 
         for sol in solutions:  # check every key-value for X,Y, and if they're less than 0 or greater than the side length of the large square then toss the data
             for key in sol.keys():
