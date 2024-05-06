c5bbc6705ea8813c8a8c4fdb02cdf05ad7a2af4c
Forrest G <forrestcgile@gmail.com>
2024-05-06 10:49:01 -0600

    main fixes

--- Project12Final.py
+++ Project12Final.py
@@ -168,6 +168,7 @@ class Robot:
 
         for sol in solutions:  # check every key-value for X,Y, and if they're less than 0 or greater than the side length of the large square then toss the data
             for key in sol.keys():
+                print(f"sol: {sol} - key: {key}")
                if isinstance(sol.get(key),complex):
                    solutions.remove(sol)
 
